c3ed8b6cccf8c871c9c8f2ae51156f90f56ea496
xalcon <xalcon@xalcon.net>
2019-01-24 22:59:32 +0100

    :hankey:

--- src/app/preload.ts
+++ src/app/preload.ts
@@ -2,11 +2,22 @@ import Fs from "fs";
 import { remote } from "electron";
 console.log("Preloading");
 
-declare const button1:HTMLButtonElement;
+declare const btnPlay:HTMLButtonElement;
+declare const btnOptions:HTMLButtonElement;
+declare const btnOptionsDone:HTMLButtonElement;
 declare const titleBarButton_minimize:HTMLButtonElement;
 declare const titleBarButton_restore:HTMLButtonElement;
 declare const titleBarButton_close:HTMLButtonElement;
 declare const document:Document;
+
+function showContainer(selector:string)
+{
+    document.querySelectorAll(".content-container").forEach(c => {
+        c.classList.add("hidden");
+    });
+
+    document.querySelector(selector)!.classList.remove("hidden");
+}
 
 onload = () =>
 {
@@ -16,24 +27,23 @@ onload = () =>
     });
 
     titleBarButton_restore.addEventListener('click', e => {
+        (document.activeElement as HTMLButtonElement).blur();
         const window = remote.getCurrentWindow()
         if(window.isMaximized()){
             window.unmaximize()
         } else {
             window.maximize()
         }
-
-        document.body.focus();
     });
 
     titleBarButton_minimize.addEventListener('click', e => {
+        (document.activeElement as HTMLButtonElement).blur();
         const window = remote.getCurrentWindow()
         window.minimize()
-
-        document.body.focus();
     });
 
     document.querySelectorAll("progress").forEach(bar => {
+        const label = document.querySelector(`.label span[data-for=${bar.id}][data-type=progress]`) as HTMLLabelElement;
         setInterval(() => {
             if(Math.random() > 0.5)
             {
@@ -41,6 +51,15 @@ onload = () =>
                 if(bar.value >= bar.max)
                     bar.value = 0;
             }
+            label.innerHTML = `${bar.value} / ${bar.max}`;
         }, 30);
-    })
+    });
+
+    document.querySelectorAll(".content-container").forEach(c => {
+        c.classList.add("hidden");
+    });
+    document.querySelector(".content-container:first-child")!.classList.remove("hidden");
+
+    btnOptions.addEventListener("click", () => showContainer("#options"));
+    btnOptionsDone.addEventListener("click", () => showContainer("#overview"));
 }
